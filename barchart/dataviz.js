73569a49bc74eeea510294a8d6a5a9b4217d5db2
Diego <diego.lgrip@gmail.com>
2020-10-25 22:09:38 +0100

    Refactor: separating BarChart from Visualization

--- barchart/dataviz.js
+++ barchart/dataviz.js
@@ -1,62 +1,94 @@
-function descendent(a, b){ return b.data.height - a.data.height }
-function ascendent(a, b){ return a.data.height - b.data.height }
+/* --------------- Chart -------------------*/
+class BarChart{
+    constructor(barw, maxBarh, colorScale, offset, marginPercentage, svg){
+        this.barw = barw;
+        this.maxBarh = maxBarh;
+        this.colorScale = colorScale;
+        this.offset = offset;
+        this.svg = svg;
+        this.chart = undefined;
 
-function rearrange(ui, chart, fn){
-    chart = chart.sort(fn);
-    chart.forEach((x, i) => {
-        x.svg.animate()
-            .dy(calcTranslation(ui, x, i))
-            .after(() => update(ui, x, i))
-    });
-}
+        this.barMarg = this.barw / marginPercentage;
+    }
 
-function calcTranslation(ui, d, j){
-    // translate from position i to j
-    return calcY(ui, j) - d.data.y;
-}
+    enter(data){
+        const bars = this.asBars(data);
+        this.chart = bars.map(x => this.drawBar(x));
+        return this;
+    }
 
-function update(ui, x, i){
+    drawBar(data){
+        let rect = this.svg.rect(data.height, data.width)
+            .x(190)
+
+        let lable = this.svg.text(data.name)
+            .font({ anchor: 'end' })
+            .dx(170)
+            .addClass('lable');
+
+
+        let bar = this.svg.group()
+            .add(rect)
+            .add(lable)
+            .attr({ fill: data.color })
+            .y(data.y)
+            .addClass('bar');
+
+        return { data: data, svg: bar };
+    }
+
+    rearrange(fn){
+        chart = this.chart.sort(fn);
+        chart.forEach((x, i) => {
+            x.svg.animate()
+                .dy(this.calcTranslation(x, i))
+                .after(() => this.update(x, i))
+        });
+    }
+    calcTranslation(d, j){
+        // translate from position i to j
+        return this.calcY(j) - d.data.y;
+    }
+
+    update(x, i){
         x.data.i = i;
-        x.data.y = calcY(ui, i);
+        x.data.y = this.calcY(i);
+    }
+
+    calcY(i){ return this.offset + i * (this.barw + this.barMarg); }
+
+    asBars(data){
+        const scale = this.scaleLinear(data.map(x => x.height));
+        let bar = (x, i) => {
+            return { name: x.name
+                   , i: i
+                   , width: this.barw
+                   , height: scale * x.height
+                   , y: this.calcY(i)
+                   , color: this.scaleColor(x.cat) }
+        }
+        return data.map((x, i) => bar(x, i));
+    }
+
+    scaleLinear(data){ return  this.maxBarh/max(data) }
+    scaleColor(cat) { return this.colorScale[cat]; }
+
 }
 
-function shuffle(a,b){ return 0.5 - Math.random(); }
 
-function height(ui){ return ui.height; }
-function width(ui){ return maxn(ui) * (barw(ui) + barMarg(ui)) * barMarg(ui); }
-function offset(ui){ return ui.offset }
-function maxn(ui){ return ui.n; }
-function set(ui){ return ui.set; }
-function barw(ui){ return ui.barw; }
-function maxBarh(ui){ return ui.maxBarH; }
-function barMarg(ui){ return barw(ui)/margin(UI); }
-function margin(ui) { return ui.margin }
-function addButton(ui, fn){ ui.buttons.push(fn) }
-function buttons(ui) { return ui.buttons; }
-function colorScale(ui, cat) { return ui.colorScale[cat]; }
-function calcY(ui, i){ return offset(ui) + i * (barw(ui) + barMarg(ui)); }
+
 
 function scale(minData, maxData, min, max){
     return (max - min) / (maxData - minData);
 }
-
 function max(data){
     let m = data[0];
     data.forEach(x => { if (x > m) m = x });
     return m;
 }
 
-function linearScale(ui, data){ return  maxBarh(ui)/max(data) }
 
-function asBars(ui, data){
-    const scale = linearScale(ui, data.map(x => x.height));
-    function bar(x, i){
-        return { name: x.name
-               , i: i
-               , width: barw(ui)
-               , height: scale * x.height
-               , y: calcY(ui, i)
-               , color: colorScale(ui, x.cat) }
-    }
-    return data.map((x, i) => bar(x, i));
-}
+function descendent(a, b){ return b.data.height - a.data.height }
+
+function ascendent(a, b){ return a.data.height - b.data.height }
+function shuffle(a,b){ return 0.5 - Math.random(); }
--- barchart/visualization.js
+++ barchart/visualization.js
@@ -3,8 +3,11 @@ class Visualization{
         this.buttons = [];
         this.data = {};
         this.chart = {};
-    }
+        this.svg = {};
 
+}
+
+    setSVG(svg){ this.svg = svg; return this; }
     setButtons(callbacks){
         callbacks.forEach(x => this.buttons.push(x));
         return this;
@@ -22,28 +25,29 @@ class Visualization{
         return this;
     }
 
-    rearrange(fn){
-        chart = this.chart.sort(fn);
-        chart.forEach((x, i) => {
-            x.svg.animate()
-                .dy(calcTranslation(this.ui, x, i))
-                .after(() => update(this.ui, x, i))
-        });
-    }
-
     draw(){
-        this.chart = this.data.map(d => drawBar(d));
+        this.svg = SVG('#chart').size(width(this.ui), height(this.ui));
+        this.barChart = new BarChart(
+             22,
+            520,
+            ['#313962', '#5f6caf', '#95ce8e', '#e65a28'],
+            100,
+            10,
+            this.svg,
+        );
+        this.barChart.enter(this.data);
         this.renderButtons()
         return this;
     }
 
+
     renderButtons(){
     let n = 0;
     this.buttons.forEach(fn => {
-        svg.rect(60, 40)
+        this.svg.rect(60, 40)
             .fill('#999')
             .move(100 + n * 80, 20)
-            .click(() => this.rearrange(fn));
+            .click(() => this.barChart.rearrange(fn));
         n++;
     });
 }
@@ -53,4 +57,12 @@ class Visualization{
         console.log("buttons:", this.buttons);
         return this;
     }
+
 }
+
+function height(ui){ return ui.height; }
+function width(ui){ return maxn(ui) * (barw(ui) + barMarg(ui)) * barMarg(ui); }
+function barw(ui){ return ui.barw; }
+function margin(ui) { return ui.margin }
+function barMarg(ui){ return barw(ui)/margin(UI); }
+function maxn(ui){ return ui.n; }
