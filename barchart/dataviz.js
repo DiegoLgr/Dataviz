4ab27b299769fbd56183f2b5946f387e2b12b1de
Diego <diego.lgrip@gmail.com>
2021-03-22 09:52:12 +0100

    .

--- barchart/dataviz.js
+++ barchart/dataviz.js
@@ -1,105 +1,105 @@
-/* --------------- Chart -------------------*/
-class BarChart{
-    constructor(barw, maxBarh, colorScale, offset, barMargin,  maxn){
-        this.barw = barw;
-        this.maxBarh = maxBarh;
-        this.colorScale = colorScale;
-        this.offset = offset;
-        this.barMarg = barMargin;
-        this.maxn = maxn;
+/* --------------- Chart ------------------- */
+class BarChart {
+  constructor (barw, maxBarh, colorScale, offset, barMargin, maxn) {
+    this.barw = barw
+    this.maxBarh = maxBarh
+    this.colorScale = colorScale
+    this.offset = offset
+    this.barMarg = barMargin
+    this.maxn = maxn
 
-        this.chart = undefined;
-        this.svg = undefined;
-        this.bars = undefined;
+    this.chart = undefined
+    this.svg = undefined
+    this.bars = undefined
+  }
+
+  getWidth () {
+    return this.maxn * (this.barw + this.barMarg) * this.barMarg
+  }
+
+  setSVG (svg) { this.svg = svg }
+
+  enter (data) {
+    this.bars = this.asBars(data)
+    return this
+  }
+
+  draw () {
+    this.chart = this.bars.map(x => this.drawBar(x))
+    return this
+  }
+
+  drawBar (data) {
+    const rect = this.svg.rect(data.height, data.width)
+      .x(190)
+
+    const lable = this.svg.text(data.name)
+      .font({ anchor: 'end' })
+      .dx(170)
+      .addClass('lable')
+
+    const bar = this.svg.group()
+      .add(rect)
+      .add(lable)
+      .attr({ fill: data.color })
+      .y(data.y)
+      .addClass('bar')
+
+    return { data: data, svg: bar }
+  }
+
+  rearrange (fn) {
+    chart = this.chart.sort(fn)
+    chart.forEach((x, i) => {
+      x.svg.animate()
+        .dy(this.calcTranslation(x, i))
+        .after(() => this.update(x, i))
+    })
+  }
+
+  calcTranslation (d, j) {
+    // translate from position i to j
+    return this.calcY(j) - d.data.y
+  }
+
+  update (x, i) {
+    x.data.i = i
+    x.data.y = this.calcY(i)
+  }
+
+  calcY (i) { return this.offset + i * (this.barw + this.barMarg) }
+
+  asBars (data) {
+    const scale = this.scaleLinear(data.map(x => x.height))
+    const bar = (x, i) => {
+      return {
+        name: x.name,
+        i: i,
+        width: this.barw,
+        height: scale * x.height,
+        y: this.calcY(i),
+        color: this.scaleColor(x.cat)
+      }
     }
-    getWidth(){
-        return this.maxn * (this.barw + this.barMarg) * this.barMarg;
-    }
-    setSVG(svg){ this.svg = svg }
+    return data.map((x, i) => bar(x, i))
+  }
 
-    enter(data){
-        this.bars = this.asBars(data);
-        return this;
-    }
-    draw(){
-        this.chart = this.bars.map(x => this.drawBar(x));
-        return this;
-    }
+  scaleLinear (data) { return this.maxBarh / max(data) }
+  scaleColor (cat) { return this.colorScale[cat] }
 
-    drawBar(data){
-        let rect = this.svg.rect(data.height, data.width)
-            .x(190)
-
-        let lable = this.svg.text(data.name)
-            .font({ anchor: 'end' })
-            .dx(170)
-            .addClass('lable');
-
-
-        let bar = this.svg.group()
-            .add(rect)
-            .add(lable)
-            .attr({ fill: data.color })
-            .y(data.y)
-            .addClass('bar');
-
-        return { data: data, svg: bar };
-    }
-
-    rearrange(fn){
-        chart = this.chart.sort(fn);
-        chart.forEach((x, i) => {
-            x.svg.animate()
-                .dy(this.calcTranslation(x, i))
-                .after(() => this.update(x, i))
-        });
-    }
-    calcTranslation(d, j){
-        // translate from position i to j
-        return this.calcY(j) - d.data.y;
-    }
-
-    update(x, i){
-        x.data.i = i;
-        x.data.y = this.calcY(i);
-    }
-
-    calcY(i){ return this.offset + i * (this.barw + this.barMarg); }
-
-    asBars(data){
-        const scale = this.scaleLinear(data.map(x => x.height));
-        let bar = (x, i) => {
-            return { name: x.name
-                   , i: i
-                   , width: this.barw
-                   , height: scale * x.height
-                   , y: this.calcY(i)
-                   , color: this.scaleColor(x.cat) }
-        }
-        return data.map((x, i) => bar(x, i));
-    }
-
-    scaleLinear(data){ return  this.maxBarh/max(data) }
-    scaleColor(cat) { return this.colorScale[cat]; }
-
-    log(){ console.log(this) }
-
+  log () { console.log(this) }
 }
 
-
-
-
-function scale(minData, maxData, min, max){
-    return (max - min) / (maxData - minData);
+function scale (minData, maxData, min, max) {
+  return (max - min) / (maxData - minData)
 }
-function max(data){
-    let m = data[0];
-    data.forEach(x => { if (x > m) m = x });
-    return m;
+function max (data) {
+  let m = data[0]
+  data.forEach(x => { if (x > m) m = x })
+  return m
 }
 
+function descendent (a, b) { return b.data.height - a.data.height }
 
-function descendent(a, b){ return b.data.height - a.data.height }
-
-function ascendent(a, b){ return a.data.height - b.data.height }
-function shuffle(a,b){ return 0.5 - Math.random(); }
+function ascendent (a, b) { return a.data.height - b.data.height }
+function shuffle (a, b) { return 0.5 - Math.random() }
--- barchart/visualization.js
+++ barchart/visualization.js
@@ -1,43 +1,42 @@
-class Visualization{
-    // This class shold be resposible of drawing all elements in the visualization,
-    constructor(height, barchart){
-        this.height = height;
-        this.barChart = barchart
+export class Visualization {
+// This class shold be resposible of drawing all elements in the visualization,
+  constructor (height, barchart) {
+    this.height = height
+    this.barChart = barchart
 
-        this.buttons = [];
-        this.svg = {};
-}
-    setButtons(callbacks){
-        callbacks.forEach(x => this.buttons.push(x));
-        return this;
-    }
+    this.buttons = []
+    this.svg = {}
+  }
 
-    draw(){
-        this.svg = SVG('#chart');
-        this.barChart.setSVG(this.svg);
-        this.svg.size(this.barChart.getWidth(), this.height);
-        this.barChart.draw();
-        this.renderButtons()
-        return this;
-    }
+  setButtons (callbacks) {
+    callbacks.forEach(x => this.buttons.push(x))
+    return this
+  }
 
+  draw () {
+    this.svg = SVG('#chart')
+    this.barChart.setSVG(this.svg)
+    this.svg.size(this.barChart.getWidth(), this.height)
+    this.barChart.draw()
+    this.renderButtons()
+    return this
+  }
 
-    renderButtons(){
-    let n = 0;
+  renderButtons () {
+    let n = 0
     this.buttons.forEach(fn => {
-        this.svg.rect(60, 40)
-            .fill('#999')
-            .move(100 + n * 80, 20)
-            .click(() => this.barChart.rearrange(fn));
-        n++;
-    });
+      this.svg.rect(60, 40)
+        .fill('#999')
+        .move(100 + n * 80, 20)
+        .click(() => this.barChart.rearrange(fn))
+      n++
+    })
+  }
+
+  log () {
+    console.log('hola')
+    console.log('data:', this.data)
+    console.log('buttons:', this.buttons)
+    return this
+  }
 }
-
-    log(){ console.log("hola");
-        console.log("data:", this.data);
-        console.log("buttons:", this.buttons);
-        return this;
-    }
-
-}
-
